85074b5b7e1361443723bccc13fdd4f287a0aefb
t-graski <tobias.graski@gmx.at>
2020-10-07 17:19:42 +0200

    Add blink and stop animation

--- app.js
+++ app.js
@@ -42,16 +42,46 @@ let cpuGaug2 = Gauge(document.getElementById("batteryC"), {
 });
 speed = 0;
 battery = 100;
+let isBrake = false;
 cpuGauge.setValue(speed);
 cpuGaug2.setValue(battery)
-//cpuGauge.setValueAnimated(260, 5);
-
-
 
 window.onkeydown = function input({ keyCode }) {
+  if (keyCode == 81) {
+    if (!leftBlink) {
+      left();
+      leftNormalID = setInterval(leftNormal, 500);
+      leftID = setInterval(left, 1000);
+      leftBlink = true;
+    } else {
+      clearInterval(leftNormalID);
+      clearInterval(leftID);
+      leftNormal();
+      leftBlink = false;
+    }
+  }
+  if (keyCode == 69) {
+    if (!rightBlink) {
+      right();
+      rightNormalID = setInterval(rightNormal, 500);
+      rightID = setInterval(right, 1000);
+      rightBlink = true;
+    } else {
+      clearInterval(rightNormalID);
+      clearInterval(rightID);
+      rightNormal();
+      rightBlink = false;
+    }
+  }
+  if (keyCode == 83) {
+    if (speed > 0 && !isBrake)
+      isBrake = true;
+      brakeID = setInterval(brake, 175);
+  }
   console.log(keyCode);
   if (battery > 0) {
     if (keyCode == 87) {
+      isBrake = false;
       if (speed < 260)
         cpuGauge.setValueAnimated(speed += 0.5);
     }
@@ -59,9 +89,16 @@ window.onkeydown = function input({ keyCode }) {
 }
 setInterval(slowDown, 150);
 function slowDown() {
-
   if (speed >= 0)
     cpuGauge.setValueAnimated(speed--);
+}
+function brake() {
+  if (speed > 0 && isBrake) {
+    cpuGauge.setValueAnimated(speed -= .4); 
+  } 
+  if(speed < 0) {
+    isBrake = false;
+  }
 }
 
 setInterval(reduceBatteryPercentage, 5000);
@@ -79,12 +116,22 @@ function reduceBatteryPercentage() {
 }
 function time() {
   const date = new Date();
-  document.querySelector('#time').innerHTML = date.getHours() + ":" + date.getMinutes();
+  document.querySelector('#time').innerHTML = date.getMinutes() < 10 ? date.getHours() + ":0" + date.getMinutes() : date.getHours() + ":" + date.getMinutes();
 }
 function date() {
   const date = new Date();
-  document.querySelector('#date').innerHTML = date.getDay() + "/" + date.getMonth() + "/" + date.getFullYear()
-  
+  document.querySelector('#date').innerHTML = date.getDate() + "/" + Number(date.getMonth() + 1) + "/" + date.getFullYear()
+}
+function welcome() {
+  const date = new Date();
+  if (date.getHours() > 5 && date.getHours() < 13)
+    document.querySelector('#header').innerHTML = 'Good Morning';
+  if (date.getHours() > 13 && date.getHours < 18)
+    document.querySelector('#header').innerHTML = 'Good Afternoon';
+  if (date.getHours() > 18 && date.getHours() < 23)
+    document.querySelector('#header').innerHTML = 'Good Evening'
+  if ((date.getHours() == 24) || (date.getHours > 0 && date.getHours() < 5))
+    document.querySelector('#header').innerHTML = 'Good Night';
 }
 async function loadTemperature() {
   try {
@@ -94,8 +141,32 @@ async function loadTemperature() {
       document.querySelector('#temp').innerHTML = Math.round(((body.list[0].main.temp - 273.15) + Number.EPSILON) * 100) / 100 + "°C"
     }
   } catch (error) {
-  
+
   }
 }
 setInterval(date, 10);
 setInterval(time, 10);
+
+let rightBlink = false;
+let leftBlink = false;
+
+let leftNormalID;
+let leftID;
+
+let rightNormalID;
+let rightID;
+
+let brakeID;
+
+function left() {
+  document.querySelector('#lArrow').src = 'arrow_yellow.png';
+}
+function leftNormal() {
+  document.querySelector('#lArrow').src = 'arrow.png';
+}
+function right() {
+  document.querySelector('#rArrow').src = 'arrow_yellow.png';
+}
+function rightNormal() {
+  document.querySelector('#rArrow').src = 'arrow.png';
+}
